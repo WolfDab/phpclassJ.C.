2a2a9472eeaaf9bc793c5a4e5a51d24731e245c6
J.C <BuildingWolf100@outlook.com>
2024-01-24 21:11:14 -0600

    initial

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>J.C.'s Homepage</title>
+    <link rel="stylesheet" type="text/css" href="base.css">
+</head>
+<body>
+<header><h1>J.C.'s Webpage</h1></header>
+<nav>
+    <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="/">Loop</a></li>
+        <li><a href="/">Countdown</a></li>
+        <ul>
+</nav>
+<main>
+    <img src="2pg2s7.jpg" alt="my brain making this website">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</main>
+<footer>&copy; 20xx WolfDab</footer>
+</body>
+</html>
